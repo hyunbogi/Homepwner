6afbe2d52a68740005f4b17ab58fa81731612b8a
Hyunbok Lee <hyunbogi@gmail.com>
2012-09-17 20:44:06 +0900

    Create project

---
+++ Source/AppDelegate/HomepwnerAppDelegate.m
@@ -0,0 +1,31 @@
+//
+//  HomepwnerAppDelegate.m
+//  Homepwner
+//
+//  Created by Hyunbok Lee on 12. 9. 17..
+//  Copyright (c) 2012년 Hyunbok Lee. All rights reserved.
+//
+
+#import "AppDelegate/HomepwnerAppDelegate.h"
+
+@implementation HomepwnerAppDelegate
+
+@synthesize window = window_;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    [[self window] makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {}
+
+- (void)applicationWillTerminate:(UIApplication *)application {}
+
+@end
---
+++ Source/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Hyunbok Lee on 12. 9. 17..
+//  Copyright (c) 2012년 Hyunbok Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
